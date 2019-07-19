da49d51993bd2766df0086ff35aebb197a3aac09
DIENDERE Amos <amos.diendere@yahoo.fr>
2019-07-19 16:00:21 +0200

    Finished TestTasks

--- src/test/java/com/eql/project_libreplan_group2/TestTasks.java
+++ src/test/java/com/eql/project_libreplan_group2/TestTasks.java
@@ -31,6 +31,23 @@ public class TestTasks {
 		pageMain.createTask("Tache2-P1", "10");
 		pageMain.createTask("Tache3-P1", "20");
 		pageMain.createTask("Tache4-P1", "8");
+		pageMain.checkTasksList("Tache1-P1","Tache2-P1","Tache3-P1","Tache4-P1");
+		pageMain.moveTaskDown("Tache1-P1");
+		Thread.sleep(200);
+		pageMain.checkTasksList("Tache2-P1","Tache1-P1","Tache3-P1","Tache4-P1");
+		pageMain.moveTaskUp("Tache3-P1");
+		Thread.sleep(200);
+		pageMain.checkTasksList("Tache2-P1","Tache3-P1","Tache1-P1","Tache4-P1");
+		pageMain.editTask("Tache1-P1", "T1", 5, null);
+		Thread.sleep(200);
+		pageMain.editTask("Tache2-P1", "T2", 8, null);
+		Thread.sleep(200);
+		pageMain.editTask("Tache3-P1", "T3", null, 3);
+		Thread.sleep(200);
+		pageMain.editTask("Tache4-P1", "T4", null, 5);
+		Thread.sleep(200);
+		pageMain.saveProject();
+		pageMain.gotoProjectPlanning();
 	}
 
 }
